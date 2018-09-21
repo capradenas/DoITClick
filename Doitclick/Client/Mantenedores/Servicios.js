6a551a280e11ace6f1221fff070366daf4451a59
Carlos Pradenas <carlpradenas@gmail.com>
2018-09-21 13:42:37 -0300

    Correcciones de servicios

--- Doitclick/Client/Mantenedores/Servicios.js
+++ Doitclick/Client/Mantenedores/Servicios.js
@@ -26,48 +26,70 @@ function render(){
         var _tdUM = $("<td>").append(material.unidadMedida)
         _trMaterial.append(_tdUM);
 
+        var _tdCNT = $("<td>").append(material.cantidad)
+        _trMaterial.append(_tdCNT);
+
         var _tdPrecio = $("<td>").append(material.precio.toMoney())
         _trMaterial.append(_tdPrecio);
 
-        var _tdStockA = $("<td>").append(material.stock)
-        _trMaterial.append(_tdStockA);
-
-
-
+        var _tdTotal = $("<td>").append((material.precio * material.cantidad).toMoney())
+        _trMaterial.append(_tdTotal);
+        /*var _tdStockA = $("<td>").append(material.stock)
+        _trMaterial.append(_tdStockA);*/
         $("#listado > tbody").append(_trMaterial);
     });
 }
 
 $(function () {
             
-    
-
-
     $('.btn-guardar-material').on('click', function(){
-        const material = {
-            materialId: $('#material').val(),
-            cantidad: $("#cantidad").val(),
-            nombre: $('#material option:selected').text(),
-            stock: $('#material option:selected').data('stocka'),
-            unidadMedida: $('#material option:selected').data('um'),
-            precio: parseInt($('#material option:selected').data('valor')),
-            valorTotal: parseInt($('#material option:selected').data('valor')) * parseInt($("#cantidad").val()) 
+
+        console.log($('#material').val(), $("#cantidad").val())
+        if ($('#material').val() != "" && $("#cantidad").val() != ""){
+            const material = {
+                materialId: $('#material').val(),
+                cantidad: $("#cantidad").val(),
+                nombre: $('#material option:selected').text(),
+                stock: $('#material option:selected').data('stocka'),
+                unidadMedida: $('#material option:selected').data('um'),
+                precio: parseInt($('#material option:selected').data('valor')),
+                valorTotal: parseInt($('#material option:selected').data('valor')) * parseInt($("#cantidad").val())
+            }
+
+
+            $('#material').val("");
+            $("#cantidad").val("");
+
+
+            $.niftyNoty({
+                type: 'info',
+                container: "#mensajero-default",
+                html: '<p class="alert-message">Material Agregado</p>',
+                timer: 2000,
+                closeBtn: false
+            });
+            modelo.materiales.push(material);
+            render();
         }
-        
-        modelo.materiales.push(material);
-
-
-        render();
-
-        console.log(modelo);
+        else 
+        {
+            $.niftyNoty({
+                type: 'warning',
+                container: "#mensajero-default",
+                html: '<p class="alert-message">Debes rellenar los campos</p>',
+                timer: 2000,
+                closeBtn: false
+            });
+        }
     });
 
 
+    $("#material").on("change", function(){
+        var um = $('#material option:selected').data('um');
+        $("#desc-unity").text(um);
+    });
 
 
-
-
-    
     $('#frm-ingreso-servicios').on("submit", function () {
         
         var mdl = $(this).serializeFormJSON();
@@ -78,10 +100,7 @@ $(function () {
             url: "/api/mantenedor/servicios",
             data: JSON.stringify(modelo),
             contentType: "application/json; charset=utf-8",
-            dataType: "json",
             success: function (data) { 
-                console.log(data); 
-
                 $.niftyNoty({
                     type: "success",
                     container : "floating",
@@ -90,8 +109,6 @@ $(function () {
                     closeBtn : false,
                     timer : 5000
                 });
-
-
                 location.reload();
                 
             },
